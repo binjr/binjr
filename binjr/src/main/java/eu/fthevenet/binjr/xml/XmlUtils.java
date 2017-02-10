eb8480f07720f72a51a5e8f2402abc2dc02e52b5
Frederic Thevenet <thevenet.fred@free.fr>
2017-02-10 11:30:25 +0100

    prepared /docs folder

--- binjr/src/main/java/eu/fthevenet/binjr/xml/XmlUtils.java
+++ binjr/src/main/java/eu/fthevenet/binjr/xml/XmlUtils.java
@@ -1,6 +1,8 @@
 package eu.fthevenet.binjr.xml;
 
-import org.xml.sax.*;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
@@ -15,18 +17,38 @@ import java.io.InputStream;
 import java.io.StringReader;
 
 /**
+ * A collections of convenience methods to help with serialization and deserialization of XML to and from Java objects.
+ *
  * @author Frederic Thevenet
  */
 public class XmlUtils {
-    public static  <T> T unmarshal(Class<T> docClass, InputStream inputStream) throws JAXBException {
-        return unmarshal(docClass, new StreamSource(inputStream));
+    /**
+     * Deserialize the XML content of a stream into a Java object of the specified type.
+     *
+     * @param docClass    The class of the object to unmarshall the XML as
+     * @param inputStream An input stream containing the XML to deserialize
+     * @param <T>         The type of object to unmarshall the XML as
+     * @return The deserialized object
+     * @throws JAXBException if an error occurs during deserialization
+     */
+    public static <T> T deSerialize(Class<T> docClass, InputStream inputStream) throws JAXBException {
+        return deSerialize(docClass, new StreamSource(inputStream));
     }
 
-    public static  <T> T unmarshal(Class<T> docClass, String xmlString) throws JAXBException {
-        return unmarshal(docClass, new StreamSource(new StringReader(xmlString)));
+    /**
+     * Deserialize the XML content of a string into a Java object of the specified type.
+     *
+     * @param docClass  The class of the object to unmarshall the XML as
+     * @param xmlString The XML to deserialize, as a string
+     * @param <T>       The type of object to unmarshall the XML as
+     * @return The deserialized object
+     * @throws JAXBException if an error occurs during deserialization
+     */
+    public static <T> T deSerialize(Class<T> docClass, String xmlString) throws JAXBException {
+        return deSerialize(docClass, new StreamSource(new StringReader(xmlString)));
     }
 
-    private static  <T> T unmarshal(Class<T> docClass, StreamSource source) throws JAXBException {
+    private static <T> T deSerialize(Class<T> docClass, StreamSource source) throws JAXBException {
         String packageName = docClass.getPackage().getName();
         JAXBContext jc = JAXBContext.newInstance(packageName);
         Unmarshaller u = jc.createUnmarshaller();
@@ -34,11 +56,19 @@ public class XmlUtils {
         return doc.getValue();
     }
 
+    /**
+     * Returns a {@link SAXSource} for the provided {@link InputStream} that explicitly forfeit external DTD validation
+     *
+     * @param in the {@link InputStream} for the {@link SAXSource}
+     * @return a {@link SAXSource} for the provided {@link InputStream} that explicitly forfeit external DTD validation
+     * @throws SAXException                 if a SAX error occurs
+     * @throws ParserConfigurationException if a configuration error occurs
+     */
     public static Source toNonValidatingSAXSource(InputStream in) throws SAXException, ParserConfigurationException {
         SAXParserFactory spf = SAXParserFactory.newInstance();
         spf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
         XMLReader xmlReader = spf.newSAXParser().getXMLReader();
         InputSource inputSource = new InputSource(in);
-       return new SAXSource(xmlReader, inputSource);
+        return new SAXSource(xmlReader, inputSource);
     }
 }
