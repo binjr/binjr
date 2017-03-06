2f9a3cd52e4f779a3496bd76b8814c8cece83758
Frederic Thevenet <thevenet.fred@free.fr>
2017-03-06 20:20:58 +0100

    removed useless public abstract from interface

--- binjr/src/main/java/eu/fthevenet/binjr/data/adapters/DataAdapter.java
+++ binjr/src/main/java/eu/fthevenet/binjr/data/adapters/DataAdapter.java
@@ -22,7 +22,7 @@ public interface DataAdapter<T extends Number> extends Serializable {
      * @return a hierarchical view of all the individual bindings exposed by the underlying source.
      * @throws DataAdapterException if an error occurs while retrieving bindings.
      */
-    public abstract TreeItem<TimeSeriesBinding<T>> getBindingTree() throws DataAdapterException;
+    TreeItem<TimeSeriesBinding<T>> getBindingTree() throws DataAdapterException;
 
     /**
      * Gets raw data from the source as an output stream, for the time interval specified.
@@ -34,47 +34,47 @@ public interface DataAdapter<T extends Number> extends Serializable {
      * @return the size of the stream (if applicable).
      * @throws DataAdapterException if an error occurs while retrieving data from the source.
      */
-    public abstract long getData(String path, Instant begin, Instant end, OutputStream out) throws DataAdapterException;
+    long getData(String path, Instant begin, Instant end, OutputStream out) throws DataAdapterException;
 
     /**
      * Gets the encoding used to decode textual data sent by the source.
      *
      * @return the encoding used to decode textual data sent by the source.
      */
-    public abstract String getEncoding();
+    String getEncoding();
 
     /**
      * Gets the id of the time zone used to record dates in the source.
      *
      * @return the id of the time zone used to record dates in the source.
      */
-    public abstract ZoneId getTimeZoneId();
+    ZoneId getTimeZoneId();
 
     /**
      * Gets the {@link DataParser} used to produce {@link eu.fthevenet.binjr.data.timeseries.TimeSeries} from the source.
      *
      * @return the {@link DataParser} used to produce {@link eu.fthevenet.binjr.data.timeseries.TimeSeries} from the source.
      */
-    public abstract DataParser<T> getParser();
+    DataParser<T> getParser();
 
     /**
      * Gets the name of the source.
      *
      * @return the name of the source.
      */
-    public abstract String getSourceName();
+    String getSourceName();
 
     /**
      * Returns a map of all parameters required to establish a connection to the underlying data source
      *
      * @return a map of all parameters required to establish a connection to the underlying data source
      */
-    public abstract Map<String, String> getParams();
+    Map<String, String> getParams();
 
     /**
      * Sets the parameters required to establish a connection to the underlying data source
      *
      * @param params the parameters required to establish a connection to the underlying data source
      */
-    public abstract void setParams(Map<String, String> params);
+    void setParams(Map<String, String> params);
 }
