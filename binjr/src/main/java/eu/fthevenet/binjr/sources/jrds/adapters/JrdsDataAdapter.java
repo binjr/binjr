113e193fc513474822b298275c8e5ab66a60e359
Frederic Thevenet <thevenet.fred@free.fr>
2017-04-17 20:13:50 +0200

    I suspect that removing the dummy tree branch during lazy loading of graph description in JRDS adapter is the cause for the infinite loop in WinAccessible.navigate(), so now the dummy node is recycled as the first of the graphdesc. This might be troublesome if a call to graphdesc returns nothing (or nothing plotable) as this would cause an empty node to linger on the parent leaf. No really dangerous but not very clean either... Also there's no guarantie that this is the root cause for the issue. If not the nuclear option of disabling the accessibilty is still there.

--- binjr/src/main/java/eu/fthevenet/binjr/sources/jrds/adapters/JrdsDataAdapter.java
+++ binjr/src/main/java/eu/fthevenet/binjr/sources/jrds/adapters/JrdsDataAdapter.java
@@ -215,7 +215,7 @@ public class JrdsDataAdapter extends SimpleCachingDataAdapter<Double> {
             // add a dummy node so that the branch can be expanded
             newBranch.getChildren().add(new TreeItem<>(null));
             // add a listener so that bindings for individual datastore are added lazily to avoid
-            // dozens of individual call to "graphdesc" servlet when the tree is built.
+            // dozens of individual call to "graphdesc" when the tree is built.
             newBranch.expandedProperty().addListener(new ChangeListener<Boolean>() {
                 @Override
                 public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
@@ -223,14 +223,23 @@ public class JrdsDataAdapter extends SimpleCachingDataAdapter<Double> {
                         try {
                             Graphdesc graphdesc = getGraphDescriptor(currentPath);
                             newBranch.setValue(bindingFactory.of(tree.getValue().getTreeHierarchy(), newBranch.getValue().getLegend(), graphdesc, currentPath, JrdsDataAdapter.this));
+                           boolean isDummyRecycled = false;
                             for (int i = 0; i < graphdesc.seriesDescList.size(); i++) {
                                 String graphType = graphdesc.seriesDescList.get(i).graphType;
                                 if (!"none".equalsIgnoreCase(graphType) && !"comment".equalsIgnoreCase(graphType)) {
-                                    newBranch.getChildren().add(new TreeItem<>(bindingFactory.of(tree.getValue().getTreeHierarchy(), graphdesc, i, currentPath, JrdsDataAdapter.this)));
+                                    TimeSeriesBinding<Double> binding = bindingFactory.of(tree.getValue().getTreeHierarchy(), graphdesc, i, currentPath, JrdsDataAdapter.this);
+                                    // I suspect that removing the dummy tree branch is the cause for the infinite loop in WinAccessible.navigate(),
+                                    // so now the dummy node is recycled as the first of the graphdesc.
+                                    // This might be troublesome if a call to graphdesc returns nothing (or nothing plotable) as this would cause an empty node
+                                    // to linger on the parent leaf. No really dangerous but not very clean either...
+                                    if (!isDummyRecycled && newBranch.getChildren().size() > 0){
+                                        newBranch.getChildren().get(0).setValue(binding);
+                                        isDummyRecycled =true;
+                                    } else {
+                                        newBranch.getChildren().add(new TreeItem<>(binding));
+                                    }
                                 }
                             }
-                            //remove dummy node
-                            newBranch.getChildren().remove(0);
                             // remove the listener so it isn't executed next time node is expanded
                             newBranch.expandedProperty().removeListener(this);
                         } catch (Exception e) {
