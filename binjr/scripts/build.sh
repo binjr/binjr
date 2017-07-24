ce6d2782fd9221e5871a7cdefbfaae3778f22119
Frederic Thevenet <thevenet.fred@free.fr>
2017-07-24 11:41:06 +0200

    [ci release] try 12

--- binjr/scripts/build.sh
+++ binjr/scripts/build.sh
@@ -5,7 +5,8 @@ if [[ $TRAVIS_COMMIT_MESSAGE == *"[ci release]"* ]]; then
     git checkout master
     cd binjr
     echo "Start Maven release"
-    mvn --batch-mode release:prepare release:perform -Dresume=false --settings target/travis/settings.xml
+    cat ./target/travis/settings.xml
+    mvn --batch-mode release:prepare release:perform -Dresume=false --settings "./target/travis/settings.xml"
 else
     cd binjr
     echo "Start Maven test"
