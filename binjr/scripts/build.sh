ffe4b7dd29820f8541482acf2e22afacf0b6c6d9
Frederic Thevenet <thevenet.fred@free.fr>
2017-07-26 12:13:15 +0200

    Github release on snapshot test 4

--- binjr/scripts/build.sh
+++ binjr/scripts/build.sh
@@ -4,10 +4,10 @@ if [[ -z "$TRAVIS_TAG" && $TRAVIS_COMMIT_MESSAGE == *"[ci release]"* ]]; then
    #"Explicitly switch to master to avoid disjointed HEAD"
     git checkout master
     cd binjr
-    echo "Start Maven release"
+    echo "*** RELEASE ***"
     mvn --batch-mode release:prepare release:perform -Dresume=false --settings "./target/travis/settings.xml"
 else
     cd binjr
-    echo "Start Maven package"
-    mvn deploy
+    echo "*** SNAPSHOT ***"
+    mvn deploy --settings "./target/travis/settings.xml"
 fi
