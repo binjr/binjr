f79cd307829bd0bd5686e7847ffea974527006a2
Frederic Thevenet <thevenet.fred@free.fr>
2017-07-26 14:36:07 +0200

    [ci release] v1.0.16

--- binjr/scripts/build.sh
+++ binjr/scripts/build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -ev
 if [[ -z "$TRAVIS_TAG" && $TRAVIS_COMMIT_MESSAGE == *"[ci release]"* ]]; then
-   #"Explicitly switch to master to avoid disjointed HEAD"
+    #Explicitly switch to master to avoid detached HEAD
     git checkout master
     cd binjr
     echo "*** RELEASE ***"
-    mvn --batch-mode release:prepare release:perform -Dresume=false --settings "./target/travis/settings.xml"
+    mvn --batch-mode release:prepare release:perform -Dresume=false --settings "./target/travis/settings.xml" -P binjr-release
 else
     cd binjr
     echo "*** SNAPSHOT ***"
-    mvn deploy --settings "./target/travis/settings.xml"
+    mvn deploy --settings "./target/travis/settings.xml" -P binjr-snapshot
 fi
