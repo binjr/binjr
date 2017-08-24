4956f7d880f7f7cf55e902e730cafcb0d40e681e
Frederic Thevenet <thevenet.fred@free.fr>
2017-08-24 14:55:18 +0200

    [ci release] Version 1.0.16

--- binjr/scripts/post_build.sh
+++ binjr/scripts/post_build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -ev
 if [[ -z "$TRAVIS_TAG" && $TRAVIS_COMMIT_MESSAGE == *"[ci release]"* ]]; then
+    echo  "Authorization: Bearer $APPVEYOR_TOKEN" '"accountName":"$SCM_USERNAME"'
     echo "*** Trigger AppVeyor build on release ***"
     curl -H "Authorization: Bearer $APPVEYOR_TOKEN" -H "Content-Type: application/json" -X POST -d '{"accountName":"$SCM_USERNAME","projectSlug":"binjr","branch":"master"}' https://ci.appveyor.com/api/builds
 fi
