83fa454471e8882f2f1c48369ec2cdf0c87a202e
Frederic Thevenet <thevenet.fred@free.fr>
2017-08-25 11:10:04 +0200

    [ci release] Version 1.0.16

--- binjr/scripts/build.sh
+++ binjr/scripts/build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-echo "TAG=$TRAVIS_TAG"
-echo "MSG=$TRAVIS_COMMIT_MESSAGE"
 set -ev
+#Skip Maven release plugin commits
+if git show -s HEAD | grep -F -q "[maven-release-plugin]" ; then echo "skip maven-release-plugin commit" && exit 0 ; fi
+
 if [[ -z "$TRAVIS_TAG" && $TRAVIS_COMMIT_MESSAGE == *"[ci release]"* ]]; then
     #Explicitly switch to master to avoid detached HEAD
     git checkout master
--- binjr/scripts/post_build.sh
+++ binjr/scripts/post_build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -ev
-if [[ -z "$TRAVIS_TAG" && $TRAVIS_COMMIT_MESSAGE == *"[ci release]"* ]]; then
-    currentCommitId="$(git rev-parse HEAD)"
-    echo $currentCommitId
-    echo "*** Trigger AppVeyor build on release ***"
-    curl -H "Authorization: Bearer $APPVEYOR_TOKEN" -H "Content-Type: application/json" -X POST -d '{"accountName":"'$SCM_USERNAME'","projectSlug":"binjr","branch":"master","commitId": "'$currentCommitId'"}' https://ci.appveyor.com/api/builds
-fi
+#if [[ -z "$TRAVIS_TAG" && $TRAVIS_COMMIT_MESSAGE == *"[ci release]"* ]]; then
+#    currentCommitId="$(git rev-parse HEAD)"
+#    echo $currentCommitId
+#    echo "*** Trigger AppVeyor build on release ***"
+#    curl -H "Authorization: Bearer $APPVEYOR_TOKEN" -H "Content-Type: application/json" -X POST -d '{"accountName":"'$SCM_USERNAME'","projectSlug":"binjr","branch":"master","commitId": "'$currentCommitId'"}' https://ci.appveyor.com/api/builds
+#fi
