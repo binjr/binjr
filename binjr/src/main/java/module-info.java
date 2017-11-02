c8c537a0eafa893941490ac0aaf74f9a86b2d4ce
Frederic Thevenet <thevenet.fred@free.fr>
2017-11-02 11:14:24 +0100

    Adding module-info and Java 9 support

---
+++ binjr/src/main/java/module-info.java
@@ -0,0 +1,25 @@
+/**
+ * Created by FTT2 on 20/06/2017.
+ */
+module eu.fthevenet.binjr {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.xml;
+    requires javafx.swing;
+    requires log4j.api;
+    requires controlsfx;
+    requires jfxutils;
+    requires java.xml.bind;
+    requires jaxb.java.time.adapters;
+    requires java.prefs;
+    requires httpclient;
+    requires gson;
+    requires httpcore;
+    requires jdk.security.auth;
+    requires javafx.web;
+    requires xstream;
+    exports eu.fthevenet.binjr;
+    exports eu.fthevenet.binjr.controllers;
+    opens eu.fthevenet.binjr.controllers;
+    exports eu.fthevenet.util.javafx.controls;
+}
