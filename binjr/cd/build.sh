6cb02a4d0d34c3f882b3935f5a236126d98f7b58
Frederic Thevenet <fthevenet@users.noreply.github.com>
2018-09-28 18:23:04 +0200

    New strategy regarding native packaging following the retirement of javapackager

--- binjr/cd/build.sh
+++ binjr/cd/build.sh
@@ -1,24 +1,27 @@
 #!/usr/bin/env bash
 set -ev
-#Skip Maven release plugin commits
-if git show -s HEAD | grep -F -q "[maven-release-plugin]" ; then
-    if [[ "$TRAVIS_OS_NAME" == "osx" && $TRAVIS_COMMIT_MESSAGE == *"prepare release"* ]]; then
-        cd binjr
-        mvn clean package --settings "./cd/maven_settings.xml" -P buildNativeBundles
-    else
-        echo "skip maven-release-plugin commit"
-    fi
-else
-    gpg --fast-import ./binjr/cd/codesigning.asc
-    if [[ -z "$TRAVIS_TAG" &&  "$TRAVIS_OS_NAME" == "linux" && $TRAVIS_COMMIT_MESSAGE == *"[ci release]"* ]]; then
-        echo "*** RELEASE ***"
-         #Explicitly switch to master to avoid detached HEAD
-        git checkout master
-        cd binjr
-        mvn --batch-mode release:prepare release:perform -Dresume=false --settings "./cd/maven_settings.xml" -P binjr-release,buildNativeBundles,sign,build-extras
-    else
-        echo "*** SNAPSHOT ***"
-        cd binjr
-        mvn clean deploy  --settings "./cd/maven_settings.xml" -P binjr-snapshot,sign,build-extras
-    fi
-fi
+  mvn clean deploy  --settings "./cd/maven_settings.xml" -P binjr-snapshot, build-native-bundle, bundle-linux
+
+
+##Skip Maven release plugin commits
+#if git show -s HEAD | grep -F -q "[maven-release-plugin]" ; then
+#    if [[ "$TRAVIS_OS_NAME" == "osx" && $TRAVIS_COMMIT_MESSAGE == *"prepare release"* ]]; then
+#        cd binjr
+#        mvn clean package --settings "./cd/maven_settings.xml" -P buildNativeBundles
+#    else
+#        echo "skip maven-release-plugin commit"
+#    fi
+#else
+#    gpg --fast-import ./binjr/cd/codesigning.asc
+#    if [[ -z "$TRAVIS_TAG" &&  "$TRAVIS_OS_NAME" == "linux" && $TRAVIS_COMMIT_MESSAGE == *"[ci release]"* ]]; then
+#        echo "*** RELEASE ***"
+#         #Explicitly switch to master to avoid detached HEAD
+#        git checkout master
+#        cd binjr
+#        mvn --batch-mode release:prepare release:perform -Dresume=false --settings "./cd/maven_settings.xml" -P binjr-release,buildNativeBundles,sign,build-extras
+#    else
+#        echo "*** SNAPSHOT ***"
+#        cd binjr
+#        mvn clean deploy  --settings "./cd/maven_settings.xml" -P binjr-snapshot,sign,build-extras
+#    fi
+#fi
