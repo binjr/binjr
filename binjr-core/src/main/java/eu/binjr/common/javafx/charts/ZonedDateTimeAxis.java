8058197d090e500b9f22782d610d279e1e579db4
Frederic Thevenet <thevenet.fred@free.fr>
2019-03-12 11:37:12 +0100

    Updated copyright notices

--- binjr-core/src/main/java/eu/binjr/common/javafx/charts/ZonedDateTimeAxis.java
+++ binjr-core/src/main/java/eu/binjr/common/javafx/charts/ZonedDateTimeAxis.java
@@ -2,6 +2,7 @@
  * The MIT License (MIT)
  *
  * Copyright (c) 2013, Christian Schudt
+ * Copyright (c) 2016-2019, Frederic Thevenet
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
