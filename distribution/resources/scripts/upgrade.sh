cabadd1609ec73bd376a7cd06c9c701f6c32de42
Jérémie Roquet <jroquet@arkanosis.net>
2019-06-10 18:49:11 +0200

    Adapt the Linux upgrade script to work on macOS as well
    This has been only been tested on FreeBSD so far, but hopefully, it will work
    the same way on macOS.

--- distribution/resources/scripts/upgrade.sh
+++ distribution/resources/scripts/upgrade.sh
@@ -1,20 +1,28 @@
 #! /bin/sh
 
 (
-    echo "[$(date --iso-8601=seconds)] Updating binjr to version ${NEW_VERSION}" &&
-        cp -rl "./${OLD_VERSION}" "./${NEW_VERSION}" &&
-        while read file; do
-            test -f "./${NEW_VERSION}/$file" &&
-                rm "./${NEW_VERSION}/$file"
-        done < "./${OLD_VERSION}/.installed" &&
+    echo "[$(date -Iseconds)] Updating binjr to version ${NEW_VERSION}" &&
         tar xzf "${PACKAGE}" "${NEW_VERSION}" &&
+        (
+            cd "./${OLD_VERSION}" &&
+                find . -mindepth 1 |
+                    sed -n 's@^\./@@p' |
+                    grep -vxFf "./.installed" |
+                    while read file; do
+                        if [ -f "$file" ] && ! [ -f "../${NEW_VERSION}/$file" ]; then
+                            mkdir -p "../${NEW_VERSION}/$(dirname "$file")" &&
+                                ln "$file" "../${NEW_VERSION}/$file" ||
+                                    exit 1
+                        fi
+                    done
+        ) &&
         ln -s "./${NEW_VERSION}/binjr" "./new_binjr" &&
         mv "./new_binjr" "./binjr" && # atomic upgrade
         rm -rf "./${OLD_VERSION}" &&
-	rm "./upgrade" &&
+        rm "./upgrade" &&
         rm "${PACKAGE}" &&
-        echo "[$(date --iso-8601=seconds)] binjr succesfully updated to version ${NEW_VERSION}" ||
-        echo "[$(date --iso-8601=seconds)] Unable to update binjr to version ${NEW_VERSION}"
+        echo "[$(date -Iseconds)] binjr succesfully updated to version ${NEW_VERSION}" ||
+        echo "[$(date -Iseconds)] Unable to update binjr to version ${NEW_VERSION}"
 ) >> "binjr-install.log" 2>&1
 
 if "${RESTART}"; then
