6817b219b59060e4a7d6f8569dbd7c53b80dbeb3
Jérémie Roquet <jroquet@arkanosis.net>
2019-08-07 16:13:47 +0200

    Avoid date flags that do not work under FreeBSD / macOS

--- distribution/platforms/linux/resources/scripts/upgrade.sh
+++ distribution/platforms/linux/resources/scripts/upgrade.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 (
-    echo "[$(date -Iseconds)] Updating binjr to version ${NEW_VERSION}" &&
+    echo "[$(date '+%Y-%m-%dT%H:%M:%S%z')] Updating binjr to version ${NEW_VERSION}" &&
         tar xzf "${PACKAGE}" "${NEW_VERSION}" &&
         (
             cd "./${OLD_VERSION}" &&
@@ -21,8 +21,8 @@
         rm -rf "./${OLD_VERSION}" &&
         rm "./upgrade" &&
         rm "${PACKAGE}" &&
-        echo "[$(date -Iseconds)] binjr succesfully updated to version ${NEW_VERSION}" ||
-        echo "[$(date -Iseconds)] Unable to update binjr to version ${NEW_VERSION}"
+        echo "[$(date '+%Y-%m-%dT%H:%M:%S%z')] binjr succesfully updated to version ${NEW_VERSION}" ||
+        echo "[$(date '+%Y-%m-%dT%H:%M:%S%z')] Unable to update binjr to version ${NEW_VERSION}"
 ) >> "binjr-install.log" 2>&1
 
 if "${RESTART}"; then
