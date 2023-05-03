a15870ce7fe37eb1c9b8aea42cbdc971279ff9f7
Frederic Thevenet <thevenet.fred@free.fr>
2023-05-03 18:11:04 +0200

    Code cleanup in AesHelper.java

--- binjr-core/src/main/java/eu/binjr/common/io/AesHelper.java
+++ binjr-core/src/main/java/eu/binjr/common/io/AesHelper.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2022 Frederic Thevenet
+ *    Copyright 2022-2023 Frederic Thevenet
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -23,6 +23,7 @@ import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -32,7 +33,9 @@ import java.util.Base64;
 
 public class AesHelper {
 
+    private static final String AES = "AES";
     private static final String AES_CBC_PKCS_5_PADDING = "AES/CBC/PKCS5Padding";
+    private static final String PBKDF_2_WITH_HMAC_SHA_256 = "PBKDF2WithHmacSHA256";
     private static final int IV_LEN = 16;
 
     public static String encrypt(String input, SecretKey key)
@@ -44,9 +47,7 @@ public class AesHelper {
         return Base64.getEncoder().encodeToString(IOUtils.concat(iv.getIV(), cipherText));
     }
 
-
-    public static String decrypt(String input, SecretKey key)
-            throws GeneralSecurityException {
+    public static String decrypt(String input, SecretKey key) throws GeneralSecurityException {
         Cipher cipher = Cipher.getInstance(AES_CBC_PKCS_5_PADDING);
         var b = IOUtils.split(Base64.getDecoder().decode(input), IV_LEN);
         var iv = new IvParameterSpec(b.first());
@@ -55,36 +56,31 @@ public class AesHelper {
         return new String(plainText);
     }
 
+    public static SecretKey getKeyFromEncoded(String encoded) {
+        byte[] buffer = Base64.getDecoder().decode(encoded);
+        return new SecretKeySpec(buffer, AES);
+    }
 
     public static SecretKey generateKey(int n) throws NoSuchAlgorithmException {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        KeyGenerator keyGenerator = KeyGenerator.getInstance(AES);
         keyGenerator.init(n);
         return keyGenerator.generateKey();
     }
 
-    public static SecretKey getKeyFromEncoded(String encoded){
-        byte[] buffer = Base64.getDecoder().decode(encoded);
-        return new SecretKeySpec(buffer, "AES");
-    }
-
     public static String generateEncodedKey(int n) throws NoSuchAlgorithmException {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-        keyGenerator.init(n);
-        return Base64.getEncoder().encodeToString(keyGenerator.generateKey().getEncoded());
+        return Base64.getEncoder().encodeToString(generateKey(n).getEncoded());
     }
 
-    public static SecretKey getKeyFromPassword(String password, String salt) {
-        SecretKeyFactory factory = null;
-        try {
-            factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+    public static SecretKey deriveKeyFromPassword(String password, String salt)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+        return deriveKeyFromPassword(password.toCharArray(), salt.getBytes(StandardCharsets.UTF_8));
+    }
 
-            KeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 65536, 256);
-            SecretKey secret = new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
-            return secret;
-        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-            e.printStackTrace();
-        }
-        return null;
+    public static SecretKey deriveKeyFromPassword(char[] password, byte[] salt)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+        SecretKeyFactory factory = SecretKeyFactory.getInstance(PBKDF_2_WITH_HMAC_SHA_256);
+        KeySpec spec = new PBEKeySpec(password, salt, 65536, 256);
+        return new SecretKeySpec(factory.generateSecret(spec).getEncoded(), AES);
     }
 
     public static IvParameterSpec generateIv() {
